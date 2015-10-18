03b93c0f42ecc72eb46804fc7bef2fbc4fb86402
凌翔 <553299576@qq.com>
2015-10-18 16:16:36 +0800

    Revert "create obj"
    This reverts commit 94fe2fd7a7a1be0b323d9cfc65c9c3640dfd2d89.

--- System/PageInfo.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace System;
-class PageInfo {
-
-    public $num = 0; //当前页码
-    public $psize = 0; //每页显示条数
-    public $dataSize = 0; //总记录数
-
-}
--- html/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('__ROOT__', dirname(dirname(__FILE__)));
-require __ROOT__.'/System/Entrance.class.php';
-\System\Entrance::action();
-
